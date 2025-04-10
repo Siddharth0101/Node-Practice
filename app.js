2a23a128c6892f744cf38e0089e346ad242a4c99
Siddharth0101 <siddsikarwar2001@gmail.com>
2025-04-10 18:11:14 +0530

    routes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser=require('body-parser')
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}))
+
 app.use('/add-product',(req, res, next) => {
-  res.send('<h1>add-product</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
 });
+
+app.post('/product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect('/')
+})
 
 app.use('/',(req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
-//Testing if i can push 
+
 app.listen(3000);
