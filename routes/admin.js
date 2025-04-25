233f60549172ce36936032a8ba7291aa6c60ce5c
Siddharth0101 <siddsikarwar2001@gmail.com>
2025-04-25 12:30:03 +0530

    controller

--- routes/admin.js
+++ routes/admin.js
@@ -3,16 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-const productController = require('../controllers/products');
+const productController = require('../controllers/product');
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product',productController );
+router.get('/add-product',productController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', );
+router.post('/add-product',productController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
